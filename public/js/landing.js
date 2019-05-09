bf18ba3c7775040ba51b8feb5fb7a76db14970e8
Casey <caseycorvino7@gmail.com>
2019-05-09 11:54:08 -0400

    alery banner

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".show-not-available").click((ev)=>{
-         alert("This ability is not in the demo ...");
+         $('.status-alert').text("Action Not Available in Demo");
     });
     $(window).scroll(function() {
         if ($(this).scrollTop() < 16) {
@@ -12,16 +12,16 @@ $(document).ready(function () {
     $('.waitlist-form').submit((ev)=>{
         ev.preventDefault();
         const inputF = $('.waitlist-form .email-input');
-        inputF.val("");
         const email = inputF.val();
+        inputF.val("");
         $.ajax({
             type: "POST",
             url: "/apply",
             data: { email : email}
         }).done(function() {
-            alert( "Confirmation sent to " + email);
+            $('.status-alert').text("Confirmation sent to " + email);
         }).fail(function() {
-            alert( "error" );
+            $('.status-alert').text("Unknown Error Sending Email");
         });
     })
 });
